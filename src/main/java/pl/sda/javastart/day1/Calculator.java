3de390c3476daa77a7c96d447696f2bbf44babf7
Sylwia Swieczkowska <sylwia_candle@yahoo.pl>
2019-01-30 15:09:47 +0100

    BigInteger BigDecimal Increment Decrement

---
+++ src/main/java/pl/sda/javastart/day1/Calculator.java
@@ -0,0 +1,23 @@
+package pl.sda.javastart.day1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadż pierwszą liczbe");
+        double firstValue = scanner.nextDouble();
+        System.out.println("Wprowadż druga liczbe");
+        double secondValue = scanner.nextDouble();
+        System.out.println("a + b = " + (firstValue + secondValue));
+        System.out.println("a - b = " + (firstValue - secondValue));
+        System.out.println("a * b = " + (firstValue * secondValue));
+        System.out.println("a / b = " + (firstValue / secondValue));
+        System.out.println("wartosc bezwzgledna z odejmowania a i b = " + Math.abs(firstValue - secondValue));
+        System.out.println("wartosc maksymalna wynosi " + Math.max(firstValue, secondValue));
+        System.out.println("wartosc minimalna wynosi " + Math.min(firstValue, secondValue));
+        System.out.println("a ^ b = " + Math.pow(firstValue, secondValue));
+        double max = Math.max(4.0, 5L);
+        System.out.println(max);
+    }
+}
---
+++ src/main/java/pl/sda/javastart/day1/MinMaxValues.java
@@ -0,0 +1,12 @@
+package pl.sda.javastart.day1;
+
+public class MinMaxValues {
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+    }
+}
