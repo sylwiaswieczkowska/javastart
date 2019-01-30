85fb1757274d69c32a6fd7d0d23a98c835056d57
Sylwia Swieczkowska <sylwia_candle@yahoo.pl>
2019-01-30 15:35:07 +0100

    Car OOP

---
+++ src/main/java/pl/sda/javastart/day4/Car.java
@@ -0,0 +1,79 @@
+package pl.sda.javastart.day4;
+
+import pl.sda.javastart.day7.ColourEnum;
+
+import java.math.BigDecimal;
+
+public class Car{
+    private ColourEnum colour;
+    private String model;
+    private String manufacture;
+    private String vin;
+    private int hp;
+    private BigDecimal basePrice;
+    private int doorNumber;
+    private CarOption[] options;
+
+    public BigDecimal summaryPrice() {
+        BigDecimal result = basePrice;
+        if (options == null) {
+            return result == null ? BigDecimal.ZERO : result;
+        }
+        for (int i = 0; i < options.length; i++) {
+            result = result.add(options[i].getOptionPrice());
+        }
+        return result;
+    }
+    public ColourEnum getColour() {
+        return colour;
+    }
+    public void setColour(ColourEnum colour) {
+        this.colour = colour;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getManufacture() {
+        return manufacture;
+    }
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+    public String getVin() {
+        return vin;
+    }
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public BigDecimal getBasePrice() {
+        return basePrice;
+    }
+    public void setBasePrice(BigDecimal basePrice) {
+        this.basePrice = basePrice;
+    }
+    public int getDoorNumber() {
+        return doorNumber;
+    }
+    public void setDoorNumber(int doorNumber) {
+        this.doorNumber = doorNumber;
+    }
+    public CarOption[] getOptions() {
+        return options;
+    }
+    public void setOptions(CarOption[] options) {
+        this.options = options;
+    }
+    @Override
+    public String toString() {
+        return model + " " + summaryPrice();
+    }
+}
---
+++ src/main/java/pl/sda/javastart/day4/CarOption.java
@@ -0,0 +1,28 @@
+package pl.sda.javastart.day4;
+
+import java.math.BigDecimal;
+
+public class CarOption {
+    private String optionName;
+    private BigDecimal optionPrice;
+    private boolean choosen;
+
+    public String getOptionName() {
+        return optionName;
+    }
+    public void setOptionName (String optionName) {
+        this.optionName = optionName;
+    }
+    public BigDecimal getOptionPrice() {
+        return optionPrice;
+    }
+    public void setOptionPrice(BigDecimal optionPrice) {
+        this.optionPrice = optionPrice;
+    }
+    public boolean isChoosen() {
+        return choosen;
+    }
+    public void setChoosen(boolean choosen) {
+        this.choosen = choosen;
+    }
+}
